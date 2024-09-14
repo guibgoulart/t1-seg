67b019370f803a58341ea64d8d95bd3dee136884
chagarumagala <elisescheibelprezzi@gmail.com>
2024-09-14 16:43:48 -0300

    remover espaco

--- kasiski.py
+++ kasiski.py
@@ -60,7 +60,7 @@ def encontrar_comprimento_chave_provavel(distancias):
 
     return comprimento_provavel
 
-# Cifra de Vigenère
+# Cifra de Vigenere
 def cifra_vigenere(texto_claro, chave):
     alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     texto_claro = texto_claro.upper()
@@ -78,16 +78,16 @@ def cifra_vigenere(texto_claro, chave):
     
     return ''.join(texto_cifrado)
 
-# Função para testar o método de Kasiski com uma chave conhecida
+# Função para testar o m etodo de Kasiski com uma chave conhecida
 def testar_metodo_kasiski():
     # Texto em claro
-    texto_claro = """On voit parfois, sur la fin de l’hiver, le jardinier soucieux de son jardin, se promener le long des espaliers et des treilles. Il examine l’état des bourgeons et du bois, interroge d’un œil attentif les mystérieuses enveloppes que va gonfler et déchirer bientôt la sève du printemps. Ces promenades où l’anxiété se mêle toujours à l’espérance, me rappellent par analogie, une autre promenade, plus troublante encore et plus intéressante, celle que peut faire à travers la jeunesse le penseur préoccupé de l’avenir. Là aussi dort, enveloppée et pourtant apparente déjà sous le voile qui la recouvre, la grande -4- question de demain. Il germe et grandit dans le cœur des jeunes, il fermente sous leur front des choses plus significatives que celles qu’essaie de deviner le jardinier sous l’écorce des bourgeons.
+    texto_claro = """On voit parfois, sur la fin de l hiver, le jardinier soucieux de son jardin, se promener le long des espaliers et des treilles. Il examine l  etat des bourgeons et du bois, interroge d un œil attentif les myst erieuses enveloppes que va gonfler et d echirer bientot la seve du printemps. Ces promenades ou l anxi et e se mêle toujours a l esp erance, me rappellent par analogie, une autre promenade, plus troublante encore et plus int eressante, celle que peut faire a travers la jeunesse le penseur pr eoccup e de l avenir. La aussi dort, envelopp ee et pourtant apparente d eja sous le voile qui la recouvre, la grande -4- question de demain. Il germe et grandit dans le cœur des jeunes, il fermente sous leur front des choses plus significatives que celles qu essaie de deviner le jardinier sous l  ecorce des bourgeons.
 
-Intéressante toujours, et toujours digne de la plus sympathique attention, la jeunesse mérite surtout de nous attirer aux époques critiques, où des changements d’orientation s’annoncent. Ne semble-t-il pas que tel soit le cas à la fin de ce siècle ? Sans doute, c’est une erreur grossière que de confondre les périodes de l’évolution humaine avec ces divisions chronologiques factices qu’on appelle des siècles. On attribue aux siècles une jeunesse et une vieillesse, on parle de leur aurore et de leur déclin. Rien ne répond moins à la réalité. Des mouvements puissants ont marqué la fin de certains siècles ; d’autres ont commencé dans le marasme et la sénilité. Il n’en est pas moins vrai qu’il peut se produire une coïncidence entre le terme d’une période historique et le terme d’un siècle. Tel est, je crois, le cas à l’heure présente. Nous avons derrière nous tout un vaste développement, dans lequel on peut remarquer, après les enthousiasmes -5- juvéniles et les efforts virils d’une brillante maturité, les hésitations et les symptômes de lassitude ordinaires à la vieillesse. Mais, comme l’humanité se renouvelle sans cesse et renaît de ses cendres, c’est au moment où les choses anciennes sont arrivées à leur maximum de décrépitude, que les choses neuves se préparent."""
+Int eressante toujours, et toujours digne de la plus sympathique attention, la jeunesse m erite surtout de nous attirer aux  epoques critiques, ou des changements d orientation s annoncent. Ne semble-t-il pas que tel soit le cas a la fin de ce siecle ? Sans doute, c est une erreur grossiere que de confondre les p eriodes de l  evolution humaine avec ces divisions chronologiques factices qu on appelle des siecles. On attribue aux siecles une jeunesse et une vieillesse, on parle de leur aurore et de leur d eclin. Rien ne r epond moins a la r ealit e. Des mouvements puissants ont marqu e la fin de certains siecles ; d autres ont commenc e dans le marasme et la s enilit e. Il n en est pas moins vrai qu il peut se produire une coïncidence entre le terme d une p eriode historique et le terme d un siecle. Tel est, je crois, le cas a l heure pr esente. Nous avons derriere nous tout un vaste d eveloppement, dans lequel on peut remarquer, apres les enthousiasmes -5- juv eniles et les efforts virils d une brillante maturit e, les h esitations et les symptomes de lassitude ordinaires a la vieillesse. Mais, comme l humanit e se renouvelle sans cesse et renait de ses cendres, c est au moment ou les choses anciennes sont arriv ees a leur maximum de d ecr epitude, que les choses neuves se pr eparent."""
     
     # Chave conhecida
     chave = "RAPHAELAMBROSIUSCOSTEAU"
-    
+    texto_claro = texto_claro.replace(" ","")
     # Cifrar o texto
     texto_cifrado = cifra_vigenere(texto_claro, chave)
     print(f"Texto cifrado: {texto_cifrado}")
